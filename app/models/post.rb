38804eadfd2ab9193da2f13265090ee334214d64
sjamescarter <stephenjamescarter@gmail.com>
2023-05-17 15:06:45 -0500

    commit tree e75db80cb1ba60caee382f0dec1c72f369f8b8b6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait, on: :create
+
+  private
+  def clickbait
+    phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+    result = false
+    phrases.each { |phrase| result = true if title.include?(phrase) }
+    if result != true
+        errors.add(:title, "Not valid clickbait")
+    end
+  end
 end
